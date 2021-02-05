5bb3238ae76a88d6557fd0dedab764701d0ebff9
Brandon_Ramos <bramos5@huskers.unl.edu>
2021-02-05 01:09:18 -0600

    Homework 4 bug found and fixed

--- Homework_4/Homework_4/Homework_4.sim/sim_1/behav/xsim/xsim.dir/Homework_4_tb_behav/obj/xsim_1.c
+++ Homework_4/Homework_4/Homework_4.sim/sim_1/behav/xsim/xsim.dir/Homework_4_tb_behav/obj/xsim_1.c
@@ -59,8 +59,8 @@ const int NumRelocateId= 9;
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/Homework_4_tb_behav/xsim.reloc",  (void **)funcTab, 9);
-	iki_vhdl_file_variable_register(dp + 4672);
-	iki_vhdl_file_variable_register(dp + 4728);
+	iki_vhdl_file_variable_register(dp + 4768);
+	iki_vhdl_file_variable_register(dp + 4824);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- Homework_4/Homework_4/Homework_4.srcs/sources_1/imports/Desktop/Homework_4.vhdl
+++ Homework_4/Homework_4/Homework_4.srcs/sources_1/imports/Desktop/Homework_4.vhdl
@@ -16,17 +16,6 @@
 -- code.  I also understand that if I knowingly give my original work to 
 -- another individual is also a violation of the honor code. 
 ------------------------------------------------------------------------- 
---library IEEE;		
---use IEEE.std_logic_1164.all; 
---use IEEE.NUMERIC_STD.ALL;
-
---entity hw4 is
---        port(	clk, reset: in std_logic; 
---		ctrl: in std_logic;
---		Q1, Q0: out unsigned(2 downto 0));
-		
---end hw4;
-
 library IEEE;		
 use IEEE.std_logic_1164.all; 
 use IEEE.NUMERIC_STD.ALL;
@@ -49,14 +38,8 @@ architecture behavior of CounterL is
 
 begin
 	
-	
 	-----------------------------------------------------------------------------
 	--		ctrl
-	--		00			hold
-	--		01			count up + mod 10
-	--		10			load D
-	--		11			synch reset
-	
 	--      0          hold
 	--      1          Q+1 mod 5
 	-----------------------------------------------------------------------------
@@ -66,73 +49,22 @@ begin
 			if (reset = '0') then
 				processQ <= (others => '0');
 				rollSynch <= '0';
-			elsif ((processQ < 5) and (ctrl = '1')) then
+			elsif ((processQ < 5-1) and (ctrl = '1')) then
+			    rollCombo <= '0';
+			     if(processQ = 5-2) then
+			         rollCombo <= '1';
+			     end if;
 				processQ <= processQ + 1;
 				rollSynch <= '0';
-			elsif ((processQ = 5) and (ctrl = '1')) then
+			elsif ((processQ = 5-1) and (ctrl = '1')) then
 				processQ <= (others => '0');
 				rollSynch <= '1';
+				rollCombo <= '0';
 			end if;
 		end if;
 	end process;
  
-	-- CSA
---	rollCombo  <= '1' when (processQ = 5) else '0';
-	roll <= rollSynch;
+	roll <= rollCombo;
 	Q <= processQ;
 	
 end behavior;
-
-
---library IEEE;		
---use IEEE.std_logic_1164.all; 
---use IEEE.NUMERIC_STD.ALL;
-
----- Entity is the higher bit of the counter 
---entity CounterH is
---	Port(	clk: in  STD_LOGIC;
---			reset : in  STD_LOGIC;
---			ctrl: in std_logic;
--- -- Only need 3 bits to go up to 5 for the mod 5 counter
---			Q: out unsigned (2 downto 0));
---end CounterH;
-
---architecture behavior of CounterH is
-	
---	signal rollSynch, rollCombo: STD_LOGIC;
---	signal processQ: unsigned (2 downto 0);
-
---begin
-	
-	
---	-----------------------------------------------------------------------------
---	--		ctrl
---	--		00			hold
---	--		01			count up + mod 10
---	--		10			load D
---	--		11			synch reset
-	
---	--      0          hold
---	--      1          Q+1 mod 5
---	-----------------------------------------------------------------------------
---	process(clk)
---	begin
---		if (rising_edge(clk)) then
---			if (reset = '0') then
---				processQ <= (others => '0');
---				rollSynch <= '0';
---			elsif ((processQ < 5) and (ctrl = '1')) then
---				processQ <= processQ + 1;
---				rollSynch <= '0';
---			elsif ((processQ = 5) and (ctrl = '1')) then
---				processQ <= (others => '0');
---				rollSynch <= '1';
---			end if;
---		end if;
---	end process;
- 
---	-- CSA
---	rollCombo  <= '1' when (processQ = 5) else '0';
---	Q <= processQ;
-	
---end behavior;
